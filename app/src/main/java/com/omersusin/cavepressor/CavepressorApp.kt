6c30e341e955b8a4d21a0e066f5bbfb963d5eee2
omersusin <tekgercek73@gmail.com>
2026-04-08 20:25:31 +0300

    chore: gradle wrapper properties and jar

---
+++ app/src/main/java/com/omersusin/cavepressor/CavepressorApp.kt
@@ -0,0 +1,7 @@
+package com.omersusin.cavepressor
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CavepressorApp : Application()
---
+++ app/src/main/java/com/omersusin/cavepressor/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.omersusin.cavepressor
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.omersusin.cavepressor.ui.screens.HistoryScreen
+import com.omersusin.cavepressor.ui.screens.HomeScreen
+import com.omersusin.cavepressor.ui.screens.SettingsScreen
+import com.omersusin.cavepressor.ui.theme.CavepressorTheme
+import com.omersusin.cavepressor.viewmodel.CompressorViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object History : Screen("history")
+    object Settings : Screen("settings")
+}
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val viewModel: CompressorViewModel = hiltViewModel()
+            val settingsState by viewModel.settingsState.collectAsState()
+
+            CavepressorTheme(
+                darkTheme = settingsState.darkTheme,
+                dynamicColor = settingsState.useDynamicColor
+            ) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.Home.route
+                    ) {
+                        composable(Screen.Home.route) {
+                            HomeScreen(
+                                viewModel = viewModel,
+                                onNavigateToSettings = {
+                                    navController.navigate(Screen.Settings.route)
+                                }
+                            )
+                        }
+                        composable(Screen.History.route) {
+                            HistoryScreen(
+                                viewModel = viewModel,
+                                onNavigateBack = { navController.popBackStack() }
+                            )
+                        }
+                        composable(Screen.Settings.route) {
+                            SettingsScreen(
+                                viewModel = viewModel,
+                                onNavigateBack = { navController.popBackStack() }
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
